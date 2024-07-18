d80762e5a42fd9c4c816b9d9cb35043c0c24519e
zusiphe214154459 <49709687+zusiphe214154459@users.noreply.github.com>
2024-07-18 21:34:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*SMOOTH SCROLL*/
+
+
+/*HERO SECTION*/
+
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+
+/*HERO SECTION ENDS*/
+
+/*PRICES SECTION */
+   
+/*PRICES SECTION ENDS*/
